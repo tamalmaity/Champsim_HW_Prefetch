90d017d3db9c4dcf197fde9a3c2dea12e81833ff
Tamal Maity <tamaldeepmaity@gmail.com>
2022-05-03 10:13:03 +0530

    Add files via upload

---
+++ obj/prefetcher/llc_prefetcher.d
@@ -0,0 +1,19 @@
+obj/prefetcher/llc_prefetcher.o: prefetcher/llc_prefetcher.cc inc/cache.h \
+ inc/memory_class.h inc/champsim.h inc/block.h inc/instruction.h \
+ inc/set.h prefetcher/bo.h prefetcher/sisb.h
+
+inc/cache.h:
+
+inc/memory_class.h:
+
+inc/champsim.h:
+
+inc/block.h:
+
+inc/instruction.h:
+
+inc/set.h:
+
+prefetcher/bo.h:
+
+prefetcher/sisb.h:
